835fd5587df2cd8df65e2e376bac4e183016bc98
Fiona Okafor <fionaokafor@gmail.com>
2020-06-06 13:28:00 +0200

    uncomment file

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,14 +4,14 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>delivr</title>
-        {{-- <link rel="stylesheet" href="app.css"> --}}
-        {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" > --}}
+        <link rel="stylesheet" href="app.css">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" >
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-        {{-- <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet"> --}}
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
         <div id="index"></div>
-        {{-- <script src="{{ asset('js/app.js') }}"></script>     --}}
+        <script src="{{ asset('js/app.js') }}"></script>    
         <script src="https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js"> </script>
         <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     </body>
